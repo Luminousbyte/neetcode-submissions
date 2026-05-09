3a544b342824c4dfbd51b777c7b6b8f925c77aad
Pinaki R. Seal <162082653+Luminousbyte@users.noreply.github.com>
2026-05-09 22:34:09 +0530

    Add: word-pattern - submission-0

---
+++ Data Structures & Algorithms/word-pattern/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        lst = []
+        for word in s.split(" "):
+            lst.append(word)
+
+        if len(pattern) != len(lst):
+            return False
+
+        hashset_pattern = set(pattern)
+        hashset_s = set(lst)
+
+        print(hashset_pattern)
+        print(hashset_s)
+
+        if len(hashset_pattern) != len(hashset_s):
+            return False
+        return True
+
