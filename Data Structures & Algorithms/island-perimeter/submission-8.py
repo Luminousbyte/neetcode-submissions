c6bd42ee55283101e1b2e8629cfb03c2e94a8bef
Pinaki R. Seal <162082653+Luminousbyte@users.noreply.github.com>
2026-05-21 16:59:19 +0530

    Add: island-perimeter - submission-8

---
+++ Data Structures & Algorithms/island-perimeter/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        ROWS, COLS =  len(grid), len(grid[0])
+        visit = set()
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        def bfs(r,c):
+            q = deque([(r,c)])
+            visit.add((r,c))
+            perimeter = 0
+
+            while q:
+                x,y = q.popleft()
+                for dx, dy in directions:
+                    nx, ny = x+dx, y+dy
+                    if (nx < 0 or ny < 0 or nx >= ROWS or ny>=COLS or grid[nx][ny] == 0):
+                        perimeter += 1
+
+                    elif (nx, ny) not in visit:
+                        visit.add((nx,ny))
+                        q.append((nx,ny))
+            return perimeter
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    return bfs(r,c)
+        return 0
