ccfd8929d02d6b4eb32fd7f796f2b3846ff3fef8
Pinaki R. Seal <162082653+Luminousbyte@users.noreply.github.com>
2026-05-30 00:48:41 +0530

    Add: clone-graph - submission-6

---
+++ Data Structures & Algorithms/clone-graph/submission-6.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        hashmap = {}
+        hashmap[node] = Node(node.val)
+        q = deque([node])
+        while q:
+            curr = q.popleft()
+            for n in curr.neighbors:
+                if n not in hashmap:
+                    hashmap[n] = Node(n.val)
+                    q.append(n)
+                hashmap[curr].neighbors.append(hashmap[n])
+        return hashmap[node]
