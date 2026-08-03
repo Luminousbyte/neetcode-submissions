46a76f4e714fde383545f9001cdf18e1411c84de
Pinaki R. Seal <162082653+Luminousbyte@users.noreply.github.com>
2026-08-03 09:54:00 +0530

    Add: duplicate-integer - submission-36

---
+++ Data Structures & Algorithms/duplicate-integer/submission-36.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        i, j = 0, 1
+        while j<len(nums):
+            if nums[i] == nums[j]:
+                return True
+            i += 1
+            j += 1
+        return False
