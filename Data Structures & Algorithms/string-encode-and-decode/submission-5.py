d47d1472e675a8c78bdd34f44c5f62a2f70d82a9
Pinaki R. Seal <162082653+Luminousbyte@users.noreply.github.com>
2026-08-28 12:44:58 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        strs_x = ""
+        for i in strs:
+            strs_x += "`"+i
+        print(strs_x)
+        return strs_x
+    def decode(self, s: str) -> List[str]:
+        lst = []
+        for word in s.split("`"):
+            lst.append(word)
+        lst.remove("")
+
+        return lst
